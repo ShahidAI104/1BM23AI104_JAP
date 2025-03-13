c60072db48784944458c9a1273d32a138bd0281d
ShahidAI104 <mohammedshahid.ai23@bmsce.ac.in>
2025-03-13 09:36:09 +0530

    Add files via upload

---
+++ gymmem.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class gymmem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the gym");
+        System.out.println("Enter number of members:");
+        int num = input.nextInt();
+
+
+        input.nextLine();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("Enter your name");
+            String name = input.nextLine();
+
+            System.out.println("Enter your age:");
+            int age = input.nextInt();
+            input.nextLine();
+            System.out.println("Enter your membership Status:");
+            boolean status = input.nextBoolean();
+            System.out.println("Enter your membership duration:");
+            int months = input.nextInt();
+            input.nextLine();
+            int price = months * 1000;
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Status: " + status);
+            System.out.println("Gym fee is Rs" + price + " for " + months + " months");
+        }
+    }
+}
+
+
+
